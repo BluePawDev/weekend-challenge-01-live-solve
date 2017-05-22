498e5caab67872cc09cfd4aecd50cf01dff1fe85
BluePawDev <jaruby@me.com>
2017-05-22 10:06:54 -0500

    Add #nextButton functionality

--- scripts/live-solve.js
+++ scripts/live-solve.js
@@ -4,6 +4,32 @@ var currentPersonIndex = 0;
 
 function onReady(){
   setCurrentShoutOut(currentPersonIndex);
+  $('#nextButton').on('click', function(){
+    // increment currentPersonIndex by one to go to next person
+    currentPersonIndex++;
+    // detect if end of array reached
+    // go back to begining if end or array reached
+    if (currentPersonIndex >= peopleArray.length) {
+      currentPersonIndex = 0;
+    }
+
+
+    // set current shout out with new index
+    setCurrentShoutOut(currentPersonIndex);
+  });
+  $('#previousButton').on('click', function(){
+    // increment currentPersonIndex by one to go to next person
+    currentPersonIndex--;
+    // detect if end of array reached
+    // go back to begining if end or array reached
+    if (currentPersonIndex >= peopleArray.length) {
+      currentPersonIndex = 0;
+    }
+
+
+    // set current shout out with new index
+    setCurrentShoutOut(currentPersonIndex);
+  });
 }
 
 function setCurrentShoutOut (currentIndex){
