b97f6e3e0712de4e1ddf0e2e47addb3ce644c126
BluePawDev <jaruby@me.com>
2017-05-22 10:11:18 -0500

    Add #previousButton functionality

--- scripts/live-solve.js
+++ scripts/live-solve.js
@@ -18,15 +18,13 @@ function onReady(){
     setCurrentShoutOut(currentPersonIndex);
   });
   $('#previousButton').on('click', function(){
-    // increment currentPersonIndex by one to go to next person
-    currentPersonIndex--;
     // detect if end of array reached
     // go back to begining if end or array reached
-    if (currentPersonIndex >= peopleArray.length) {
-      currentPersonIndex = 0;
+    if (currentPersonIndex <= 0) {
+      currentPersonIndex = peopleArray.length;
     }
-
-
+    // increment currentPersonIndex by one to go to next person
+    currentPersonIndex--;
     // set current shout out with new index
     setCurrentShoutOut(currentPersonIndex);
   });
