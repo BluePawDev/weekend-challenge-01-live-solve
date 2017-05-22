7d28f1ab07cc71634d1382d00b1fa58a757c4f86
BluePawDev <jaruby@me.com>
2017-05-22 09:57:49 -0500

    Update buttons on index.html/Add setCurrentShoutOut() in live-solve.js

--- scripts/live-solve.js
+++ scripts/live-solve.js
@@ -1,5 +1,11 @@
 $(onReady);
 
-function onReady(){
+var currentPersonIndex = 0;
 
+function onReady(){
+  setCurrentShoutOut(currentPersonIndex);
+}
+
+function setCurrentShoutOut (currentIndex){
+  console.log(peopleArray[currentIndex]);
 }
